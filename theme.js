718c5f73df544338e0d2137967c96224dc99e125
Tazkie <100460377+TazkieCT@users.noreply.github.com>
2024-11-12 11:56:33 +0700

    Mega Update
    Fixing icon and chaging theme

---
+++ theme.js
@@ -0,0 +1,140 @@
+const root = document.querySelector(":root");
+const style = getComputedStyle(root);
+
+let primaryColor = style.getPropertyValue("--primaryColor"); /* Color Logo */
+let secondaryColor = style.getPropertyValue("--secondaryColor"); /* Color Logo Teks */
+let tertiaryColor = style.getPropertyValue("--tertiaryColor"); /* Color Teks Konten */
+let quaternaryColor = style.getPropertyValue("--quaternaryColor"); /* Color Background Web */
+let quinaryColor = style.getPropertyValue("--quinaryColor"); /* Color Sub-Background Web (Lebih gelap) */
+
+const themeColorsArray = [
+    {
+        name: "default",
+        colors: {
+            primaryColor: "#FFB800",
+            secondaryColor: "#D1D0C5",
+            tertiaryColor: "#646669",
+            quaternaryColor: "#323437",
+            quinaryColor: "#2C2E31"
+        }
+    },
+    {
+        name: "arch",
+        colors: {
+            primaryColor: "#F44C7F",
+            secondaryColor: "#E9ECF0",
+            tertiaryColor: "#939EAE",
+            quaternaryColor: "#333A45",
+            quinaryColor: "#45474A"
+        }
+    },
+    {
+        name: "dark",
+        colors: {
+            primaryColor: "#EEEEEE",
+            secondaryColor: "#EEEEEE",
+            tertiaryColor: "#444444",
+            quaternaryColor: "#111111",
+            quinaryColor: "#181A1D"
+        }
+    },
+    {
+        name: "apple",
+        colors: {
+            primaryColor: "#E55050",
+            secondaryColor: "#FFAAAA",
+            tertiaryColor: "#FF6060",
+            quaternaryColor: "#6E2626",
+            quinaryColor: "#6B6E71"
+        }
+    },
+    {
+        name: "toys",
+        colors: {
+            primaryColor: "#D19D01",
+            secondaryColor: "#332800",
+            tertiaryColor: "#0061FE",
+            quaternaryColor: "#FFF2D5",
+            quinaryColor: "#6B6E71"
+        }
+    },
+    {
+        name: "snow candy",
+        colors: {
+            primaryColor: "#FF360D",
+            secondaryColor: "#000000",
+            tertiaryColor: "#B7B7B7",
+            quaternaryColor: "#FFFFFF",
+            quinaryColor: "#6B6E71"
+        }
+    }, 
+    {
+        name: "io",
+        colors: {
+            primaryColor: "#55C6F0",
+            secondaryColor: "#1D1E1E",
+            tertiaryColor: "#1E107A",
+            quaternaryColor: "#FFF9F2",
+            quinaryColor: "#6B6E71"
+        }
+    }, 
+    {
+        name: "pie",
+        colors: {
+            primaryColor: "#F17754",
+            secondaryColor: "#FFBC90",
+            tertiaryColor: "#FFE4BC",
+            quaternaryColor: "#081426",
+            quinaryColor: "#6B6E71"
+        }
+    }, 
+];
+
+const themeSpan = document.getElementById("theme");
+const modal = document.getElementById("theme-modal");
+const closeButton = document.querySelector(".close-button");
+
+function showModal() {
+    console.log("Showing modal.......");
+    modal.classList.add("show");
+    populateThemes();
+}
+
+function hideModal() {
+    modal.classList.remove("show");
+}
+
+themeSpan.addEventListener("click", showModal);
+
+closeButton.addEventListener("click", hideModal);
+window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+        hideModal();
+    }
+});
+
+function populateThemes() {
+    const themeList = document.getElementById("theme-list");
+    themeList.innerHTML = "";
+
+    themeColorsArray.forEach((theme, index) => {
+        const themeItem = document.createElement("div");
+        themeItem.textContent = theme.name;
+        themeItem.style.cursor = "pointer";
+        
+        themeItem.addEventListener("click", () => applyTheme(index));
+        
+        themeList.appendChild(themeItem);
+    });
+}
+
+function applyTheme(themeIndex) {
+    const selectedTheme = themeColorsArray[themeIndex].colors;
+    root.style.setProperty("--primaryColor", selectedTheme.primaryColor);
+    root.style.setProperty("--secondaryColor", selectedTheme.secondaryColor);
+    root.style.setProperty("--tertiaryColor", selectedTheme.tertiaryColor);
+    root.style.setProperty("--quaternaryColor", selectedTheme.quaternaryColor);
+    root.style.setProperty("--quinaryColor", selectedTheme.quinaryColor);
+
+    hideModal();
+}
